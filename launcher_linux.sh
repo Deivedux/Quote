7c7f4328cb84d157ac3d569ac6ba0b4fbba822ab
Aki <45246543+aki-jp@users.noreply.github.com>
2018-11-22 09:37:30 +0900

    Update launcher_linux.sh

--- launcher_linux.sh
+++ launcher_linux.sh
@@ -5,28 +5,28 @@ echo ""
 
 
 choices=4
-echo "1-- Launch The Bot"
-echo "2-- Launch With Auto-Restart"
-echo "3-- Update to latest version"
-echo "4-- Update to stable version"
+              echo "1-- Launch The Bot"
+              echo "2-- Launch With Auto-Restart"
+              echo "3-- Update to latest version"
+              echo "4-- Update to stable version"
 
 while [ $choice -eq 4]; do
 read choice
 if [ $choice -eq 1] ; then
   run_bot
 else
-      if [ $choice -eq 2] ; then
-        echo "Starting the bot with auto-restart.."
-        echo "-to be added"
-      else
-            if [ $choice -eq 3] ; then
-              echo "Updating to latest version.."
-            else
-                  if [ $choice -eq 4] ; then
-                    echo "Updating to stable version.."
-                  fi
+        if [ $choice -eq 2] ; then
+            echo "Starting the bot with auto-restart.."
+            echo "-to be added"
+        else
+                if [ $choice -eq 3] ; then
+                    echo "Updating to latest version.."
+                else
+                        if [ $choice -eq 4] ; then
+                            echo "Updating to stable version.."
             fi
-      fi
+        fi
+    fi
 fi
 
 run_bot() {
