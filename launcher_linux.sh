9f3cfaff57bd4d7e9f5da1e3405f1f9fae911399
Aki <45246543+aki-jp@users.noreply.github.com>
2018-11-23 11:13:29 +0900

    Cloning configs works now
    They see me rollin', they hatin'- I mean what.
    Quote/configs will now be backed up in Quote-Old and then transferred back to Quote once the stable version gets downloaded.

--- launcher_linux.sh
+++ launcher_linux.sh
@@ -23,18 +23,18 @@ else
                 else
                         if [ "$choice" = "4" ] ; then
                             echo "Updating to stable version.."
-                            if [ -d "/home/Quote" ]; then
-                                if [ -d "/home/Quote-Old" ]; then
+                            if [ -d "/home/Quote" ] ; then
+                                if [ -d "/home/Quote-Old" ] ; then
                                     rm /home/Quote-Old/* -f
-                                    mv -Ri /home/Quote/configs /home/Quote-Old
+                                    mv -v /home/Quote/configs -t /home/Quote-Old -f 
                                     rm -rf /home/Quote -f
-                                    git clone https://github.com/Deivedux/Quote.git
-                                    mv -Ri /home/Quote-Old/configs /home/Quote
+                                    git clone https://github.com/Deivedux/Quote.git /home/
+                                    mv -v /home/Quote-Old/configs -t /home/Quote -f 
                                 else
                                     sudo mkdir /home/Quote-Old
-                                    mv -Ri /home/Quote/configs /home/Quote-Old
-                                    git clone https://github.com/Deivedux/Quote.git 
-                                    mv -Ri /home/Quote-Old/configs /home/Quote
+                                    mv -v /home/Quote/configs -t /home/Quote-Old -f 
+                                    git clone https://github.com/Deivedux/Quote.git /home/
+				          mv -v /home/Quote-Old/configs -t /home/Quote -f
                                 fi
                             else
                                 git clone https://github.com/Deivedux/Quote.git
