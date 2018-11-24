ae5d38b1ea1db98c91bd8f414f5dc5e5016385c2
Aki <45246543+aki-jp@users.noreply.github.com>
2018-11-24 21:46:23 +0900

    no one should care about these its dev branch

--- autorestart_linux.sh
+++ autorestart_linux.sh
@@ -1,1 +1,15 @@
-Initiate
+#!/bin/sh
+
+if hash python3 2>/dev/null
+then
+      echo "Python3 installed."
+else
+      echo "Python3 is not installed. Refer to Linux guide!"
+      exit 1
+fi
+
+echo "Running Quote with autorestart."
+while : ; do python3 quote.py; sleep 5s; done
+echo "Done"
+
+exit 0
