28cdcbcdd9edffb920ffd40acfea27ce51ec05b3
Aki <45246543+aki-jp@users.noreply.github.com>
2018-11-24 21:56:07 +0900

    Added latest version download > Check desc
    Going to need to test if it works first

--- launcher_linux.sh
+++ launcher_linux.sh
@@ -19,35 +19,54 @@ else
             echo "-to be added"
         else
                 if [ "$choice" = "3" ] ; then
-                    echo "Updating to latest version.."
+			echo "Updating to latest version.."
+		    	if [ -d "/home/Quote" ] ; then
+		    		if [ -d "/home/Quote-Old" ] ; then
+					rm /home/Quote-Old/* -f
+					mv -v /home/Quote/configs -t /home/Quote-Old -f
+					rm -rf /home/Quote -f
+					git clone https://github.com/Deivedux/Quote/tree/dev.git /home/
+					mv -v /home/Quote-Old/configs -t /home/Quote -f
+					echo ""
+					echo "Done!"
+				else
+					sudo mkdir /home/Quote-Old
+					mv -v /home/Quote/configs -t /home/Quote-Old -f
+					git clone https://github.com/Deivedux/Quote/tree/dev.git /home/
+					mv -v /home/Quote-Old/configs -t /home/Quote -f
+					echo ""
+					echo "Done!"
+				fi
+			else
+		   
                 else
-                        if [ "$choice" = "4" ] ; then
-                            echo "Updating to stable version.."
-                            if [ -d "/home/Quote" ] ; then
-                                if [ -d "/home/Quote-Old" ] ; then
-                                    rm /home/Quote-Old/* -f
-                                    mv -v /home/Quote/configs -t /home/Quote-Old -f 
-                                    rm -rf /home/Quote -f
-                                    git clone https://github.com/Deivedux/Quote.git /home/
-                                    mv -v /home/Quote-Old/configs -t /home/Quote -f 
-				    echo ""
-				    echo "Done!"
-                                else
-                                    sudo mkdir /home/Quote-Old
-                                    mv -v /home/Quote/configs -t /home/Quote-Old -f 
-                                    git clone https://github.com/Deivedux/Quote.git /home/
-				    mv -v /home/Quote-Old/configs -t /home/Quote -f
-				    echo ""
-				    echo "Done!"
-                                fi
-                            else
-                                git clone https://github.com/Deivedux/Quote.git
-				echo ""
-				echo "Done!"
-                            fi
-            fi
-        fi
-    fi
+			if [ "$choice" = "4" ] ; then
+				echo "Updating to stable version.."
+                            	if [ -d "/home/Quote" ] ; then
+                                	if [ -d "/home/Quote-Old" ] ; then
+                                    		rm /home/Quote-Old/* -f
+                                    		mv -v /home/Quote/configs -t /home/Quote-Old -f 
+                                    		rm -rf /home/Quote -f
+                                    		git clone https://github.com/Deivedux/Quote.git /home/
+                                    		mv -v /home/Quote-Old/configs -t /home/Quote -f 
+				    		echo ""
+				    		echo "Done!"
+					else
+                                    		sudo mkdir /home/Quote-Old
+                                    		mv -v /home/Quote/configs -t /home/Quote-Old -f 
+                                    		git clone https://github.com/Deivedux/Quote.git /home/
+				    		mv -v /home/Quote-Old/configs -t /home/Quote -f
+				    		echo ""
+				    		echo "Done!"
+                                	fi
+                            	else
+                                	git clone https://github.com/Deivedux/Quote.git
+					echo ""
+					echo "Done!"
+                            	fi
+            		fi
+        	fi
+    	fi
 fi
 
 run_bot() {
