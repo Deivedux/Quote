7f3d9a7b5dede7fffdd35ca29f76ac41382dcec2
Aki <45246543+aki-jp@users.noreply.github.com>
2018-11-24 22:39:10 +0900

    Equality 4 the win (Syntax fix)

--- launcher_linux.sh
+++ launcher_linux.sh
@@ -4,25 +4,25 @@ echo "Welcome to Quote launcher for Linux!"
 echo ""
 
 choice=4
-              echo "1-- Launch The Bot"
-              echo "2-- Launch With Auto-Restart"
-              echo "3-- Update to latest version"
-              echo "4-- Update to stable version"
+	echo "1-- Launch The Bot"
+        echo "2-- Launch With Auto-Restart"
+        echo "3-- Update to latest version"
+        echo "4-- Update to stable version"
 
 while [ "$choice" = "4" ]; do
 read choice
 if [ "$choice" = "1" ] ; then
-  run_bot
+	run_bot
 else
-        if [ "$choice" = "2" ] ; then
-            echo "Starting the bot with auto-restart.."
-            sh autorestart_linux.sh
-	    exit 1
+	if [ "$choice" = "2" ] ; then
+        	echo "Starting the bot with auto-restart.."
+            	sh autorestart_linux.sh
+		exit 1
         else
-                if [ "$choice" = "3" ] ; then
+		if [ "$choice" = "3" ] ; then
 			echo "Updating to latest version.."
-		    	if [ -d "/home/Quote-Latest" ] ; then
-		    		if [ -d "/home/Quote-Old" ] ; then
+			if [ -d "/home/Quote-Latest" ] ; then
+				if [ -d "/home/Quote-Old" ] ; then
 					rm /home/Quote-Old/* -f
 					mv -v /home/Quote-Latest/configs -t /home/Quote-Old -f
 					rm -rf /home/Quote-Latest -f
@@ -80,26 +80,26 @@ else
 fi
 
 run_bot() {
-  echo "Checking requirements.."
-  if hash python3 2>/dev/null; then
-    if python -m pip install --user -r https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/requirements.txt; then
-      echo "Starting Quote.."
-      python quote.py
-    else
-      echo "Failed to install requirements!"
-      exit 254
-    fi
-  elif hash python 2>/dev/null; then
-          case "$(python --version 2>&1)" in
-                  *" 3."*)
-                          echo ""
-                          ;;
-                  *)
-                          echo "Wrong Python version! You need Python 3.5+ to run Quote!"
-                          exit
-                          ;;
-          esac
-  fi
+	echo "Checking requirements.."
+  	if hash python3 2>/dev/null; then
+  	  	if python -m pip install --user -r https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/requirements.txt; then
+      			echo "Starting Quote.."
+      			python quote.py
+    		else
+	      		echo "Failed to install requirements!"
+      			exit 254
+    		fi
+  	elif hash python 2>/dev/null; then
+		case "$(python --version 2>&1)" in
+		*" 3."*)
+			echo ""
+                	;;
+			*)
+			echo "Wrong Python version! You need Python 3.5+ to run Quote!"
+			exit
+                        ;;
+		esac
+	fi
 }
 
 done
