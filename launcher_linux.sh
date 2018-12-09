65cfafd4720ed6339ed39621d1a797ecefc76da2
Aki <45246543+aki-jp@users.noreply.github.com>
2018-12-10 03:59:32 +0900

    will work on

--- launcher_linux.sh
+++ launcher_linux.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Welcome to Quote launcher"
+echo "Welcome to Quote launcher for Linux!"
 echo ""
 
 choice=4
@@ -14,17 +14,22 @@ choice=4
 while [ "$choice" = "4" ]; do
 read choice
 if [ "$choice" = "1" ] ; then
-	curl https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/reqs.sh && sh reqs.sh
-	run_bot
+	echo ""
+	echo "Getting requirements."
+	sleep 5s
+	curl https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/reqs.sh | sh
+	echo ""
+	echo "Done!"
+	echo "Starting Bot..."
+	python3 quote.py
 else
 	if [ "$choice" = "2" ] ; then
-		curl https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/reqs.sh && sh reqs.sh
-		echo "Starting the bot with auto-restart. Please wait..."
-		sh autorestart_linux.sh
+			curl https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/reqs.sh | sh
+        	echo "Starting the bot with auto-restart.."
+            	sh autorestart_linux.sh
 		exit 1
-		else
+        else
 		if [ "$choice" = "3" ] ; then
-			curl https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/reqs.sh && sh reqs.sh
 			echo "Updating to latest version.."
 			if [ -d "/home/Quote-Latest" ] ; then
 				if [ -d "/home/Quote-Old" ] ; then
@@ -47,62 +52,62 @@ else
 				fi
 			else
 				sudo mkdir /home/Quote-Latest/
-				git clone -b dev https://github.com/Deivedux/Quote.git /home/Quote-Latest
-				echo ""
-				echo "Done!"
+			        git clone -b dev https://github.com/Deivedux/Quote.git /home/Quote-Latest
+			        echo ""
+			        echo "Done!"
 			fi
-			else
+		   
+                else
 			if [ "$choice" = "4" ] ; then
-				curl https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/reqs.sh && sh reqs.sh
 				echo "Updating to stable version.."
-					if [ -d "/home/Quote" ] ; then
-						if [ -d "/home/Quote-Old" ] ; then
-							rm /home/Quote-Old/* -f
-							mv -v /home/Quote/configs -t /home/Quote-Old -f
-							rm -rf /home/Quote -f
-							git clone https://github.com/Deivedux/Quote.git /home/
-							rm -rf /home/Quote/configs -f
-							mv -v /home/Quote-Old/configs -t /home/Quote -f
-							echo ""
-							echo "Done!"
-						else
-							sudo mkdir /home/Quote-Old
-							mv -v /home/Quote/configs -t /home/Quote-Old -f
-							git clone https://github.com/Deivedux/Quote.git /home
-							rm -rf /home/Quote/configs -f
-							mv -v /home/Quote-Old/configs -t /home/Quote -f
-							echo ""
-							echo "Done!"
-						fi
+                            	if [ -d "/home/Quote" ] ; then
+                                	if [ -d "/home/Quote-Old" ] ; then
+                                    		rm /home/Quote-Old/* -f
+                                    		mv -v /home/Quote/configs -t /home/Quote-Old -f 
+                                    		rm -rf /home/Quote -f
+                                    		git clone https://github.com/Deivedux/Quote.git /home/
+						rm -rf /home/Quote/configs -f
+                                    		mv -v /home/Quote-Old/configs -t /home/Quote -f 
+				    		echo ""
+				    		echo "Done!"
 					else
-						git clone https://github.com/Deivedux/Quote.git
-						echo ""
-						echo "Done!"
-					fi
-			fi
-		fi
-	fi
+                                    		sudo mkdir /home/Quote-Old
+                                    		mv -v /home/Quote/configs -t /home/Quote-Old -f 
+                                    		git clone https://github.com/Deivedux/Quote.git /home/
+						rm -rf /home/Quote/configs -f
+				    		mv -v /home/Quote-Old/configs -t /home/Quote -f
+				    		echo ""
+				    		echo "Done!"
+                                	fi
+                            	else
+                                	git clone https://github.com/Deivedux/Quote.git
+					echo ""
+					echo "Done!"
+                            	fi
+            		fi
+        	fi
+    	fi
 fi
 
 run_bot() {
 	echo "Checking requirements.."
-	if python3 2>/dev/null; then
-		if python -m pip install --user -r https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/requirements.txt; then
-			echo "Starting Quote.."
-			python3 quote.py
-		else
-			echo "Failed to install requirements!"
-			exit 254
-		fi
-	elif hash python 2>/dev/null; then
+  	if hash python3 2>/dev/null; then
+  	  	if python -m pip install --user -r https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/requirements.txt; then
+      			echo "Starting Quote.."
+      			python quote.py
+    		else
+	      		echo "Failed to install requirements!"
+      			exit 254
+    		fi
+  	elif hash python 2>/dev/null; then
 		case "$(python --version 2>&1)" in
 		*" 3."*)
 			echo ""
-				;;
+                	;;
 			*)
-			echo "Wrong Python version. You need Python 3.5+"
+			echo "Wrong Python version! You need Python 3.5+ to run Quote!"
 			exit
-				;;
+                        ;;
 		esac
 	fi
 }
