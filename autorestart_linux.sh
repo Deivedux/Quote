d18536babeaeffae40f148443a27cf9d50f18161
Aki <45246543+aki-jp@users.noreply.github.com>
2018-12-10 01:41:38 +0900

    Cleanup and ident fix

--- autorestart_linux.sh
+++ autorestart_linux.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-if hash python3 2>/dev/null
-      then
-            echo "Python3 installed."
-      else
-            echo "Python3 is not installed. Refer to Linux guide!"
-            exit 1
+if hash python3 2>/dev/null; then
+      echo "Python 3.5+ installed."
+      echo "Running Quote with Auto-Restart!"
+      while : ; do python3 quote.py; sleep 5s; done
+      echo "Done"
+      exit 0
+else
+      echo "Python 3.5+ is not installed. Refer to Linux guide!"
+      exit 1
 fi
-
-echo "Running Quote with autorestart."
-while : ; do python3 quote.py; sleep 5s; done
-echo "Done"
-
-exit 0
--- launcher_linux.sh
+++ launcher_linux.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Welcome to Quote launcher for Linux!"
+echo "Welcome to Quote launcher"
 echo ""
 
 choice=4
@@ -9,7 +9,7 @@ choice=4
 	echo "3-- Update to latest version"
 	echo "4-- Update to stable version"
 	echo ""
-	echo -e "• You have been given pre-ready publicly accessible program's code that already strictly follows Discord's API Terms of Service (https://discordapp.com/developers/docs/legal).\n• You are free to modify the code to your own likings, but failure to accept the license agreement(https://github.com/Deivedux/Quote/blob/master/LICENSE) will result in a blacklist from the official bot, a ban from it's official Discord server, and a possible flag on your GitHib account.\n• If you are planning on injecting malicious code into your copy of the program, you are risking a ban on your bot account, and a possible Discord account termination under Discord Staff discretion. If you will be found not banned yet, then the above punishments will apply.\n\nBy self hosting, you agree to all of the above terms."
+	echo -e "• You have been given pre-ready publicly accessible program's code that already strictly follows Discord's API Terms of Service (https://discordapp.com/developers/docs/legal).\n• You are free to modify the code to your own likings, but failure to accept the license agreement(https://github.com/Deivedux/Quote/blob/master/LICENSE) will result in a blacklist from the official bot, a ban from it's official Discord server, and a possible flag on your GitHib account.\n• If you are planning on injecting malicious code into your copy of the program, you are risking a ban on your bot account, and a possible Discord account termination under Discord Staff discretion.If you will be found not banned yet, then the above punishments will apply.\n\nBy self hosting, you agree to all of the above terms."
 
 while [ "$choice" = "4" ]; do
 read choice
@@ -17,16 +17,16 @@ if [ "$choice" = "1" ] ; then
 	run_bot
 else
 	if [ "$choice" = "2" ] ; then
-        	echo "Starting the bot with auto-restart.."
-            	sh autorestart_linux.sh
+		echo "Starting the bot with auto-restart. Please wait..."
+		sh autorestart_linux.sh
 		exit 1
-        else
+		else
 		if [ "$choice" = "3" ] ; then
 			echo "Updating to latest version.."
 			if [ -d "/home/Quote-Latest" ] ; then
 				if [ -d "/home/Quote-Old" ] ; then
 					rm /home/Quote-Old/* -f
-					mv -v /home/Quote-Latest/configs -t /home/Quote-Old -f
+					mv -v /home/Quote-Latest/configs
 					rm -rf /home/Quote-Latest -f
 					git clone -b dev https://github.com/Deivedux/Quote.git /home/Quote-Latest
 					rm -rf /home/Quote-Latest/configs -f
@@ -44,62 +44,61 @@ else
 				fi
 			else
 				sudo mkdir /home/Quote-Latest/
-			        git clone -b dev https://github.com/Deivedux/Quote.git /home/Quote-Latest
-			        echo ""
-			        echo "Done!"
+				git clone -b dev https://github.com/Deivedux/Quote.git /home/Quote-Latest
+				echo ""
+				echo "Done!"
 			fi
-		   
-                else
+			else
 			if [ "$choice" = "4" ] ; then
 				echo "Updating to stable version.."
-                            	if [ -d "/home/Quote" ] ; then
-                                	if [ -d "/home/Quote-Old" ] ; then
-                                    		rm /home/Quote-Old/* -f
-                                    		mv -v /home/Quote/configs -t /home/Quote-Old -f 
-                                    		rm -rf /home/Quote -f
-                                    		git clone https://github.com/Deivedux/Quote.git /home/
-						rm -rf /home/Quote/configs -f
-                                    		mv -v /home/Quote-Old/configs -t /home/Quote -f 
-				    		echo ""
-				    		echo "Done!"
+					if [ -d "/home/Quote" ] ; then
+						if [ -d "/home/Quote-Old" ] ; then
+							rm /home/Quote-Old/* -f
+							mv -v /home/Quote/configs -t /home/Quote-Old -f
+							rm -rf /home/Quote -f
+							git clone https://github.com/Deivedux/Quote.git /home/
+							rm -rf /home/Quote/configs -f
+							mv -v /home/Quote-Old/configs -t /home/Quote -f
+							echo ""
+							echo "Done!"
+						else
+							sudo mkdir /home/Quote-Old
+							mv -v /home/Quote/configs -t /home/Quote-Old -f
+							git clone https://github.com/Deivedux/Quote.git /home
+							rm -rf /home/Quote/configs -f
+							mv -v /home/Quote-Old/configs -t /home/Quote -f
+							echo ""
+							echo "Done!"
+						fi
 					else
-                                    		sudo mkdir /home/Quote-Old
-                                    		mv -v /home/Quote/configs -t /home/Quote-Old -f 
-                                    		git clone https://github.com/Deivedux/Quote.git /home/
-						rm -rf /home/Quote/configs -f
-				    		mv -v /home/Quote-Old/configs -t /home/Quote -f
-				    		echo ""
-				    		echo "Done!"
-                                	fi
-                            	else
-                                	git clone https://github.com/Deivedux/Quote.git
-					echo ""
-					echo "Done!"
-                            	fi
-            		fi
-        	fi
-    	fi
+						git clone https://github.com/Deivedux/Quote.git
+						echo ""
+						echo "Done!"
+					fi
+			fi
+		fi
+	fi
 fi
 
 run_bot() {
 	echo "Checking requirements.."
-  	if hash python3 2>/dev/null; then
-  	  	if python -m pip install --user -r https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/requirements.txt; then
-      			echo "Starting Quote.."
-      			python quote.py
-    		else
-	      		echo "Failed to install requirements!"
-      			exit 254
-    		fi
-  	elif hash python 2>/dev/null; then
+	if python3 2>/dev/null; then
+		if python -m pip install --user -r https://raw.githubusercontent.com/aki-jp/QuoteRequirements/master/requirements.txt; then
+			echo "Starting Quote.."
+			python3 quote.py
+		else
+			echo "Failed to install requirements!"
+			exit 254
+		fi
+	elif hash python 2>/dev/null; then
 		case "$(python --version 2>&1)" in
 		*" 3."*)
 			echo ""
-                	;;
+				;;
 			*)
-			echo "Wrong Python version! You need Python 3.5+ to run Quote!"
+			echo "Wrong Python version. You need Python 3.5+"
 			exit
-                        ;;
+				;;
 		esac
 	fi
 }
