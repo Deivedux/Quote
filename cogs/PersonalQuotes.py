a4046bc76002b49c66a5db5ce9d215e6a1f5af7e
Deivedux <36776659+Deivedux@users.noreply.github.com>
2018-12-21 02:45:29 +0200

    Added attachments

--- cogs/PersonalQuotes.py
+++ cogs/PersonalQuotes.py
@@ -13,12 +13,19 @@ with open('configs/config.json') as json_data:
 conn = sqlite3.connect('configs/QuoteBot.db')
 c = conn.cursor()
 
-def personal_embed(response, author):
+def personal_embed(db_response, author):
 	if isinstance(author, discord.Member) and author.color != discord.Colour.default():
-		embed = discord.Embed(description = response, color = author.color)
+		embed = discord.Embed(description = db_response[2], color = author.color)
 	else:
-		embed = discord.Embed(description = response)
+		embed = discord.Embed(description = db_response[2])
 	embed.set_author(name = str(author), icon_url = author.avatar_url)
+	if db_response[3] != None:
+		attachments = db_response[3].split(' | ')
+		if len(attachments) == 1 and attachments[0].lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.gifv', '.webp', '.bmp')):
+			embed.set_image(url = attachments[0])
+		else:
+			embed.add_field(name = 'Attachment(s)', value = '\n'.join(attachments))
+	embed.set_footer(text = 'Personal Quote')
 	return embed
 
 def list_embed(list_personals, author):
@@ -34,15 +41,38 @@ class PersonalQuotes:
 		self.bot = bot
 
 	@commands.command(aliases = ['padd'])
-	async def personaladd(self, ctx, trigger, *, response):
+	async def personaladd(self, ctx, trigger, *, response = None):
 		user_quotes = c.execute("SELECT Trigger FROM PersonalQuotes WHERE User = " + str(ctx.author.id)).fetchall()
 		if len(user_quotes) >= 10:
 			return await ctx.send(content = error_string + ' **You have already exceeded your personal quotes limit.**')
 		elif trigger in [i[0] for i in user_quotes]:
 			return await ctx.send(content = error_string + ' **You already have a quote with that trigger.**')
 
-		c.execute("INSERT INTO PersonalQuotes (User, Trigger, Response) VALUES (" + str(ctx.author.id) + ", '" + trigger.replace('\'', '\'\'') + "', '" + response.replace('\'', '\'\'') + "')")
-		conn.commit()
+		if not response and not ctx.message.attachments:
+			return await ctx.send(content = error_string + ' **You must include at least a response or an attachment in your message.**')
+
+		if ctx.message.attachments:
+			if len(ctx.message.attachments) == 1 and ctx.message.attachments[0].url.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.gifv', '.webp', '.bmp')):
+				try:
+					c.execute("INSERT INTO PersonalQuotes (User, Trigger, Response, Attachments) VALUES (" + str(ctx.author.id) + ", '" + trigger.replace('\'', '\'\'') + "', '" + response.replace('\'', '\'\'') + "', '" + ctx.message.attachments[0].url.replace('\'', '\'\'') + "')")
+					conn.commit()
+				except sqlite3.OperationalError:
+					c.execute("ALTER TABLE PersonalQuotes ADD COLUMN Attachments TEXT")
+
+					c.execute("INSERT INTO PersonalQuotes (User, Trigger, Response, Attachments) VALUES (" + str(ctx.author.id) + ", '" + trigger.replace('\'', '\'\'') + "', '" + response.replace('\'', '\'\'') + "', '" + ctx.message.attachments[0].url.replace('\'', '\'\'') + "')")
+					conn.commit()
+			else:
+				try:
+					c.execute("INSERT INTO PersonalQuotes (User, Trigger, Response, Attachments) VALUES (" + str(ctx.author.id) + ", '" + trigger.replace('\'', '\'\'') + "', '" + response.replace('\'', '\'\'') + "', '" + ' | '.join(['[' + attachment.filename.replace('\'', '\'\'') + '](' + attachment.url.replace('\'', '\'\'') + ')' for attachment in ctx.message.attachments]) + "')")
+					conn.commit()
+				except sqlite3.OperationalError:
+					c.execute("ALTER TABLE PersonalQuotes ADD COLUMN Attachments TEXT")
+
+					c.execute("INSERT INTO PersonalQuotes (User, Trigger, Response, Attachments) VALUES (" + str(ctx.author.id) + ", '" + trigger.replace('\'', '\'\'') + "', '" + response.replace('\'', '\'\'') + "', '" + ' | '.join(['[' + attachment.filename.replace('\'', '\'\'') + '](' + attachment.url.replace('\'', '\'\'') + ')' for attachment in ctx.message.attachments]) + "')")
+					conn.commit()
+		else:
+			c.execute("INSERT INTO PersonalQuotes (User, Trigger, Response) VALUES (" + str(ctx.author.id) + ", '" + trigger.replace('\'', '\'\'') + "', '" + response.replace('\'', '\'\'') + "')")
+			conn.commit()
 
 		await ctx.send(content = success_string + ' **Quote added.**')
 
@@ -68,7 +98,7 @@ class PersonalQuotes:
 				except discord.Forbidden:
 					pass
 
-			await ctx.send(embed = personal_embed(user_quote[2], ctx.author))
+			await ctx.send(embed = personal_embed(user_quote, ctx.author))
 
 	@commands.command(aliases = ['plist'])
 	async def personallist(self, ctx):
